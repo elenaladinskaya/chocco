514ad8153b1c1c0779ab06d58c6fc78a3a3e6dc7
Elena Ladinskaya <ladinskaya.elena@gmail.com>
2021-03-29 14:20:11 +0300

    Реализованы: слайдер, вертикальный аккордеон, слайдшоу отзывов, модальное окно

--- js/fullscreenMenu.js
+++ js/fullscreenMenu.js
@@ -1,15 +1,18 @@
 const hamburger = document.querySelector("#hamburger");
 const fullscreenMenu = document.querySelector(".fullscreen-menu");
 const close = document.querySelector(".fullscreen-menu__close")
+const body = document.body;
 
 hamburger.addEventListener("click", e => {
   e.preventDefault();
 
   fullscreenMenu.classList.add("active");
+  body.classList.add("body-active-menu");
 })
 
 close.addEventListener("click", e => {
   e.preventDefault();
 
   fullscreenMenu.classList.remove("active");
+  body.classList.remove("body-active-menu");
 })
---
+++ js/slider.js
@@ -0,0 +1,14 @@
+const slider = $('.slider__list').bxSlider({
+  pager: false,  
+  controls: false,
+});
+
+$('.slider__btn--left').click(e => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+});
+
+$('.slider__btn--right').click(e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+});
