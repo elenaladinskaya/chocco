bea1cfc5749df249f93c4c823b89509b68ed70f5
Elena Ladinskaya <ladinskaya.elena@gmail.com>
2021-04-07 19:08:46 +0300

    Исправлена некорректная работа слайдера

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,25 @@
+// bxSlider
+// (function () {
+//   const slider = $('.slider__list').bxSlider({
+//     pager: false,
+//     controls: false,
+//   });
+
+//   $('.slider__btn--left').click(e => {
+//     e.preventDefault();
+//     slider.goToPrevSlide();
+//   });
+
+//   $('.slider__btn--right').click(e => {
+//     e.preventDefault();
+//     slider.goToNextSlide();
+//   });
+// })()
+
+// slick slider
 (function () {
-  const slider = $('.slider__list').bxSlider({
-    pager: false,
-    controls: false,
-  });
-
-  $('.slider__btn--left').click(e => {
-    e.preventDefault();
-    slider.goToPrevSlide();
-  });
-
-  $('.slider__btn--right').click(e => {
-    e.preventDefault();
-    slider.goToNextSlide();
+ $('.slider__list').slick({
+    prevArrow: $('.slider__btn--left'),
+    nextArrow: $('.slider__btn--right'),
   });
 })()
